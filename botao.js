ecfcd738d540885b0ec6f01bfbafb621f8aa90c6
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2023-10-15 16:46:51 -0300

    funçoes nos buttons

---
+++ botao.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', (e) =>{
+    if (e.target.classList.contains('assistaAgora')) {
+
+            window.open('https://www.netflix.com/br/title/70143836', '_blank')
+    }
+
+    if (e.target.classList.contains('botaoCuriosidade')) {
+        
+        window.open('https://pt.wikipedia.org/wiki/Breaking_Bad', '_blank')
+
+    }
+})
+
